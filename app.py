441f7a437b85606e5a5488b074416c9eb5566370
Jojohere05 <140842575+Jojohere05@users.noreply.github.com>
2024-12-13 15:49:39 +0530

    app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from wordcloud import WordCloud
 from pathlib import Path
 import os
 st.markdown("<h1 style='text-align: center; font-weight: bold;'>🧑‍💻 Exploring Language Trends: A Feature-Based Popularity Analysiss 📊</h1>", unsafe_allow_html=True)
-file_path = Path(__file__).parent / 'data_cleaned.csv'
+file_path = 'C:/Users/Jhotika Raja/OneDrive/Desktop'
 df = None
 try:
     df = pd.read_csv(file_path)
