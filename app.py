c2a905a26a9529937c158e37a061efcb67cbbb60
Jojohere05 <2005jhotikaraja@gmail.com>
2025-02-26 15:32:49 +0530

    app.py filepath

--- app.py
+++ app.py
@@ -12,8 +12,17 @@ from sklearn.decomposition import PCA
 from wordcloud import WordCloud
 from pathlib import Path
 import os
+import gdown
 st.markdown("<h1 style='text-align: center; font-weight: bold;'>🧑‍💻 Exploring Language Trends: A Feature-Based Popularity Analysiss 📊</h1>", unsafe_allow_html=True)
-file_path = r'C:\Users\Jhotika Raja\Downloads\dpel\data_cleaned.csv'
+file_path = 'data_cleaned.csv'
+drive_file_id = "1rfvrtoEz0nEC7CUWu8XvOCNPGiSr1lJH"
+if not os.path.exists(file_path):
+    try:
+        url = f"https://drive.google.com/uc?id={drive_file_id}"
+        gdown.download(url, file_path, quiet=False)
+        st.success("Dataset downloaded successfully.")
+    except Exception as e:
+        st.error(f"Failed to download dataset: {str(e)}")
 df = None
 try:
     df = pd.read_csv(file_path)
